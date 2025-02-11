f48513f0b2c082286e3775cb34463c857092db96
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-02-11 21:47:08 +0700

    noob

--- NoName001/Assets/Scripts/Player.cs
+++ NoName001/Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float playerSpeed = 10;
-    private bool facingRight;
-    public float jumpPower = 10;
-    private float moveX;
+    public float playerSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal");
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2(x, y);
+        transform.Translate (movement * playerSpeed * Time.deltaTime);
     }
 }
