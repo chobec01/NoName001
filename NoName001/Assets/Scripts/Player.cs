d5d83f48e65b35377970f1bed48375c773627256
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-02-21 21:27:22 +0700

    noob

--- NoName001/Assets/Scripts/Player.cs
+++ NoName001/Assets/Scripts/Player.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float playerSpeed = 1f;
+    Rigidbody2D rb2D;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    private void FixedUpdate()
+    {
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis ("Vertical");
-        Vector2 movement = new Vector2(x, y);
-        transform.Translate (movement * playerSpeed * Time.deltaTime);
+        float y = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(x, y) * playerSpeed * Time.deltaTime;
+        rb2D.MovePosition(rb2D.position + movement);
     }
 }
