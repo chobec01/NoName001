16f3327e304961bb6f383ce35333775f133d8f1f
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-02-22 19:27:34 +0700

    noob

--- NoName001/Assets/Scripts/Player.cs
+++ NoName001/Assets/Scripts/Player.cs
@@ -6,11 +6,14 @@ public class Player : MonoBehaviour
 {
     public float playerSpeed = 1f;
     Rigidbody2D rb2D;
+    public int maxHealth = 10;
+    int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,5 +27,10 @@ public class Player : MonoBehaviour
         float y = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(x, y) * playerSpeed * Time.deltaTime;
         rb2D.MovePosition(rb2D.position + movement);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
