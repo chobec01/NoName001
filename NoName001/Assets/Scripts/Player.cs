8b6e02e6d421000e58a5e9dcde1e9f5e5d9f3a99
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-02-25 17:13:22 +0700

    noob

--- NoName001/Assets/Scripts/Player.cs
+++ NoName001/Assets/Scripts/Player.cs
@@ -7,14 +7,13 @@ public class Player : MonoBehaviour
     public float playerSpeed = 1f;
     Rigidbody2D rb2D;
     public int maxHealth = 10;
-    int currentHealth;
+    int currentHealth = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
-        ChangeHealth(currentHealth);
+        //currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -29,9 +28,10 @@ public class Player : MonoBehaviour
         Vector2 movement = new Vector2(x, y) * playerSpeed * Time.deltaTime;
         rb2D.MovePosition(rb2D.position + movement);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
+    public int getHealth { get { return currentHealth; } }
 }
--- NoName001/Assets/Scripts/Potion.cs
+++ NoName001/Assets/Scripts/Potion.cs
@@ -6,6 +6,21 @@ public class Potion : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("On trigger" + other);
+        Player playerController = other.GetComponent<Player>();
+        if (playerController != null && playerController.getHealth < playerController.maxHealth)
+        {
+            string potionName = gameObject.name;
+            if (potionName == "HP Potion 2")
+            {
+                playerController.ChangeHealth(1);
+            }
+            if (potionName == "HP Potion 1")
+            {
+                playerController.ChangeHealth(2);
+            }
+            Destroy(gameObject);
+        }
+        else
+            Debug.Log("Max HP");
     }
 }
