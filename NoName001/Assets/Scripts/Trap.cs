bc43fde3193d8258a02d3b589e3e59d88daf427a
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-03-13 16:16:55 +0700

    noob

--- NoName001/Assets/Scripts/Trap.cs
+++ NoName001/Assets/Scripts/Trap.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D other)
+    /*private void OnTriggerStay2D(Collider2D other)
     {
         Player playerController = other.GetComponent<Player>();
         if (playerController != null)
         {
             playerController.ChangeHealth(-1);
+        }
+    }*/
+    public int dmg;
+    Coroutine coroutine;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null && coroutine == null) 
+        {
+            coroutine = StartCoroutine(DealDmg(player));
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if(player != null && coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+    }
+
+    IEnumerator DealDmg(Player player)
+    {
+        while (true)
+        {
+            player.ChangeHealth(-1);
+            yield return new WaitForSeconds(1);
         }
     }
 }
