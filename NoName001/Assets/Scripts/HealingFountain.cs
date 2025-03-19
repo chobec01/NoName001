a2c0aa7596de098bf303e0b2ee8cf58163906ffe
chobec01 <94282363+chobec01@users.noreply.github.com>
2025-03-19 15:35:44 +0700

    noob

--- NoName001/Assets/Scripts/HealingFountain.cs
+++ NoName001/Assets/Scripts/HealingFountain.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class HealingFountain : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D other)
+    /*private void OnTriggerStay2D(Collider2D other)
     {
         Player playerController = other.GetComponent<Player>();
         if (playerController != null)
         {
             playerController.ChangeHealth(1);
+        }
+    }*/
+    public int healMount;
+    Coroutine coroutine;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null && coroutine == null)
+        {
+            coroutine = StartCoroutine(Heal(player));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if(player != null && coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+    }
+
+    IEnumerator Heal(Player player)
+    {
+        while(true)
+        {
+            player.ChangeHealth(1);
+            yield return new WaitForSeconds(1);
         }
     }
 }
